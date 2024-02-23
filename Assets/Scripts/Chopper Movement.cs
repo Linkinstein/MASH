2724f757c23502b72e1846609a8fa084a20b8fc8
Linkinstein <78932362+Linkinstein@users.noreply.github.com>
2024-02-23 20:05:31 +1100

    Revert "Revert "was adding counters but smth else bugged""
    This reverts commit 8d1ccea95d3798e6dccc827970bd9d2379be9100.

--- Assets/Scripts/Chopper Movement.cs
+++ Assets/Scripts/Chopper Movement.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 
 public class ChopperMovement : MonoBehaviour
@@ -6,29 +7,34 @@ public class ChopperMovement : MonoBehaviour
     [SerializeField] private float speed = 0.01f;
     [SerializeField] private float x = 0;
     [SerializeField] private float y = 0;
-    [SerializeField] private int maxSpeed = 0;
+
+    [SerializeField] public Boolean playing = true;
+    [SerializeField] public int holding = 0;
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+        if (playing)
+        {
+            x = Input.GetAxis("Horizontal");
+            y = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(x, y, 0);
-        move = Vector3.ClampMagnitude(move, 1f);
+            Vector3 move = new Vector3(x, y, 0);
+            move = Vector3.ClampMagnitude(move, 1f);
 
-        gameObject.transform.position += (move * speed);
-        if (gameObject.transform.position.x > 6) gameObject.transform.position = new Vector3(6, gameObject.transform.position.y, gameObject.transform.position.z);
-        if (gameObject.transform.position.x < -6) gameObject.transform.position = new Vector3(-6, gameObject.transform.position.y, gameObject.transform.position.z);
-        if (gameObject.transform.position.y > 3.5) gameObject.transform.position = new Vector3(gameObject.transform.position.x, 3.5f, gameObject.transform.position.z);
-        if (gameObject.transform.position.y < -3.5) gameObject.transform.position = new Vector3(gameObject.transform.position.x, -3.5f, gameObject.transform.position.z);
+            gameObject.transform.position += (move * speed);
+            if (gameObject.transform.position.x > 6) gameObject.transform.position = new Vector3(6, gameObject.transform.position.y, gameObject.transform.position.z);
+            if (gameObject.transform.position.x < -6) gameObject.transform.position = new Vector3(-6, gameObject.transform.position.y, gameObject.transform.position.z);
+            if (gameObject.transform.position.y > 3.5) gameObject.transform.position = new Vector3(gameObject.transform.position.x, 3.5f, gameObject.transform.position.z);
+            if (gameObject.transform.position.y < -3.5) gameObject.transform.position = new Vector3(gameObject.transform.position.x, -3.5f, gameObject.transform.position.z);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Tree"))
         {
-            Debug.Log("Tree!");
+            //playing = false;
         }
         if (other.gameObject.CompareTag("Soldier"))
         {
